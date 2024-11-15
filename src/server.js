6d367af7f98f61dc91bbb7fe1e7d50996522bf69
DuongLuc2003 <lucck131103@gmail.com>
2024-11-15 00:05:56 -0800

    update Port

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // Middleware for CORS
 app.use((req, res, next) => {
